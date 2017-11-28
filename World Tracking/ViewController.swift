19674bf8bac15d423c776badb6681b56de6a52e9
kenneth moody <km3372@gmail.com>
2017-11-28 14:27:22 -0500

    init commit

--- World Tracking/ViewController.swift
+++ World Tracking/ViewController.swift
@@ -7,12 +7,18 @@
 //
 
 import UIKit
+import ARKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sceneView: ARSCNView!
+    
+    let configration = ARWorldTrackingConfiguration()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+        self.sceneView.session.run(configration)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func addButton(_ sender: Any) {
+        let node = SCNNode()
+        node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
+        node.geometry?.firstMaterial?.diffuse.contents = UIColor.black
+        node.position = SCNVector3(0.2,0.2,0.3)
+        self.sceneView.scene.rootNode.addChildNode(node)
+        
+    }
+    
 }
 
