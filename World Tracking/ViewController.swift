9cd9da0179805a0cf8858c6c7983876516cb9a52
kenneth moody <km3372@gmail.com>
2017-11-28 17:43:22 -0500

    Adding an alpha box

--- World Tracking/ViewController.swift
+++ World Tracking/ViewController.swift
@@ -19,21 +19,36 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
         self.sceneView.session.run(configration)
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        self.sceneView.autoenablesDefaultLighting = true
     }
 
     @IBAction func addButton(_ sender: Any) {
         let node = SCNNode()
-        node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
+        node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.03)
+        node.geometry?.firstMaterial?.specular.contents = UIColor(hue: 0.1333, saturation: 1, brightness: 1, alpha: 1.0) /* #ffcc00 */
         node.geometry?.firstMaterial?.diffuse.contents = UIColor.black
-        node.position = SCNVector3(0.2,0.2,0.3)
+        let x = randomNumbers(firstNum: -0.3, secondNum: 0.3)
+        let y = randomNumbers(firstNum: -0.3, secondNum: 0.3)
+        let z = randomNumbers(firstNum: -0.3, secondNum: 0.3)
+        
+        node.position = SCNVector3(x,y,z)
         self.sceneView.scene.rootNode.addChildNode(node)
         
     }
+    @IBAction func restButton(_ sender: UIButton) {
+        self.restartSession()
+    }
     
+    func restartSession() {
+        self.sceneView.session.pause()
+        self.sceneView.scene.rootNode.enumerateChildNodes {(node, _) in
+            node.removeFromParentNode()
+        }
+        self.sceneView.session.run(configration, options: [.resetTracking, .removeExistingAnchors])
+    }
+    func randomNumbers(firstNum: CGFloat, secondNum: CGFloat) -> CGFloat {
+        return CGFloat(arc4random()) / CGFloat(UINT32_MAX) * abs(firstNum - secondNum)
+            min(firstNum, secondNum)
+    }
 }
 
